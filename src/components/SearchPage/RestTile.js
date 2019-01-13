a4db073cda5312cfd1665d5f4ee950f8a164daa4
Kunal Grover <kunalgrover05@gmail.com>
2019-01-14 00:45:40 +0530

    Add Map

--- src/components/SearchPage/RestTile.js
+++ src/components/SearchPage/RestTile.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import { Row, Col } from 'react-grid-system';
-import { BrowserRouter as Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Grid from '@material-ui/core/Grid';
 
 
 export const RestTile = (props) => {
-    console.log(props);
     return (
         <div style={{ padding: '30px' }}>
-            <Row style={{ background: 'white', border: '1px solid grey', borderRadius: '6px' }}>
-                <Col xs={11}>
+            <Grid container spacing={24}>
+                <Grid item xs={11} style={{ background: 'white', border: '1px solid grey', borderRadius: '6px' }}>
                     <div style={{ padding: '10px' }}><Link to={`/rest/${props.rest.id}`}>{props.rest.title}</Link></div>
                     <div style={{ padding: '10px' }}>{props.rest.cuisine}</div>
-                </Col>
-                <Col xs={1} style={{padding: '0px'}}>
+                </Grid>
+                <Grid item xs={1} style={{ padding: '0px' }}>
                     <img style={{ width: '100%', float: 'right' }} src={props.rest.images ? props.rest.images[0] : ''} />
-                </Col>
-            </Row>
-            <Row>
-                <Col>
+                </Grid>
+            </Grid>
+            <Grid container>
+                <Grid item xs={6}>
                     <div>{props.rest.open_closed}</div>
-                </Col>
-                <Col>
+                </Grid>
+                <Grid item xs={6}>
                     <div>{props.rest.rating}</div>
-                </Col>
-            </Row>
+                </Grid>
+            </Grid>
         </div>
     );
 
--- src/components/SearchPage/index.js
+++ src/components/SearchPage/index.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, View } from 'react';
 
 import { Query } from 'react-apollo';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { RESTAURANT_SEARCH_QUERY } from '../../graphql/queries';
+import { MapPage } from './MapPage';
+import { RestTile } from './RestTile';
+import Grid from '@material-ui/core/Grid';
 
 class SearchPage extends Component {
   render() {
@@ -23,6 +26,13 @@ class SearchPage extends Component {
           console.log('data', data);
           console.log('error', error);
 
+          const coordsArray = data.search_restaurants.results.map(x => {
+            return {
+              lat: x.lat,
+              lng: x.lon
+            }
+          });
+
           // Make sure we have data
           if (
             data.search_restaurants
@@ -30,11 +40,18 @@ class SearchPage extends Component {
             && data.search_restaurants.results.length > 0
           ) {
             return (
-              <div>
+              <Grid container>
+                <Grid item xs={4} style={{height: '100vh', overflow: 'scroll'}}>
                 {data.search_restaurants.results.map((r) => {
-                  return <div>{r.title} ({r.id})</div>;
-                })}
-              </div>
+                return <RestTile key={r.id} rest={r}></RestTile>
+              })}
+                
+                </Grid>
+                <Grid item xs={8}>
+                <MapPage coordsArray={coordsArray}></MapPage>
+                </Grid>
+
+              </Grid>
             );
           }
 
